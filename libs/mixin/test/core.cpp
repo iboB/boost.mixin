d84b8c084b4c3fb6871eb1f38873f7ebe46fdb23
Borislav Stanimirov <b.stanimirov@abv.bg>
2013-02-04 09:15:49 +0200

    simple message test

--- libs/mixin/test/core.cpp
+++ libs/mixin/test/core.cpp
@@ -20,9 +20,12 @@ class other_domain {};
 
 // some mixins and messages
 BOOST_DECLARE_MIXIN(counter);
+BOOST_DECLARE_MIXIN(type_checker);
 //BOOST_DECLARE_MIXIN(overrider);
 //BOOST_DECLARE_MIXIN(foo);
 //BOOST_DECLARE_MIXIN(bar);
+
+BOOST_MIXIN_CONST_MESSAGE_0(const void*, get_self);
 
 class counter
 {
@@ -32,6 +35,15 @@ public:
     void count_uni();
     int get_count();
     void count_multi();
+};
+
+class type_checker
+{
+public:
+    const void* get_self() const
+    {
+        return this;
+    }
 };
 
 
@@ -51,6 +63,16 @@ BOOST_AUTO_TEST_CASE(simple_inline_mutation)
 
     BOOST_CHECK(!o.has<counter>());
     BOOST_CHECK_NULL(o.get<counter>());
+}
+
+BOOST_AUTO_TEST_CASE(basic_message)
+{
+    object* o = new object;
+    mutate(o).add<type_checker>();
+
+    BOOST_CHECK_EQUAL(get_self(o), o->get<type_checker>());
+
+    delete o;
 }
 
 BOOST_AUTO_TEST_CASE(complex_apply_mutation)
@@ -90,3 +112,6 @@ BOOST_AUTO_TEST_CASE(complex_apply_mutation)
 
 
 BOOST_DEFINE_MIXIN(counter, none);
+BOOST_DEFINE_MIXIN(type_checker, get_self_msg);
+
+BOOST_MIXIN_DEFINE_MESSAGE(get_self);
