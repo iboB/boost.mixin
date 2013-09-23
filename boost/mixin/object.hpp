d6c3c6b646f7a73f95cb65514cb5aae3b57016f2
Borislav Stanimirov <b.stanimirov@abv.bg>
2013-09-24 00:22:11 +0300

    fixed include guard

--- boost/mixin/object.hpp
+++ boost/mixin/object.hpp
@@ -5,7 +5,7 @@
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 //
-#if !defined(_BOOST_MIXIN_OBJECTL_HPP_INCLUDED)
+#if !defined(_BOOST_MIXIN_OBJECT_HPP_INCLUDED)
 #define _BOOST_MIXIN_OBJECT_HPP_INCLUDED
 
 #include "global.hpp"
