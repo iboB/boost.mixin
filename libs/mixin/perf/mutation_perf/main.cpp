7a94ede40e7bee343ad802ee667b1c2769a9add4
Borislav Stanimirov <b.stanimirov@abv.bg>
2014-02-20 23:37:37 +0200

    message performance docs and wip for test

---
+++ libs/mixin/perf/mutation_perf/main.cpp
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2013-2014 Borislav Stanimirov, Zahary Karadjov
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char**)
+{
+    return 0;
+}
