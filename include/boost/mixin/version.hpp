45d4a1bd62a63f5f80930ebe63c82e16829e6161
Borislav Stanimirov <b.stanimirov@abv.bg>
2014-02-27 20:17:39 +0200

    library version info header

---
+++ include/boost/mixin/version.hpp
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2013-2014 Borislav Stanimirov, Zahary Karadjov
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//
+#if !defined(_BOOST_MIXIN_VERSION_HPP_INCLUDED)
+#define _BOOST_MIXIN_VERSION_HPP_INCLUDED
+
+/**
+ * \file
+ * Library version info
+ * Optional header file
+ */
+
+#define BOOST_MIXIN_VERSION_MAJOR 1
+#define BOOST_MIXIN_VERSION_MINOR 0
+#define BOOST_MIXIN_VERSION_SUB_MINOR 0
+
+/// Version composed of `major*10000 + minor*100 + subminor`.
+/// For example, version 2.31.11 would be 23111.
+#define BOOST_MIXIN_VERSION (BOOST_MIXIN_VERSION_MAJOR*10000 + BOOST_MIXIN_VERSION_MINOR*100 + BOOST_MIXIN_VERSION_SUB_MINOR)
+
+#endif // _BOOST_MIXIN_VERSION_HPP_INCLUDED
